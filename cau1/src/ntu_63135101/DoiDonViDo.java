6afb0fab29c9a17e112b70458267add958d3477b
phuc091003 <huuphucnguyen73@gmail.com>
2024-03-24 22:23:19 +0700

    Update DoiDonViDo.java

--- cau1/src/ntu_63135101/DoiDonViDo.java
+++ cau1/src/ntu_63135101/DoiDonViDo.java
@@ -11,9 +11,13 @@ import javax.swing.SwingUtilities;
 public class DoiDonViDo {
 
 	
-	public static void main(String[] args) {
-		
+	public static void main (String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run() {
+				form app = new form();
+				app.showform();
+			}
+		});
 	}
 }
-
 

e53c0cd01f68fd6a63ff8a98898880cb32934969
phuc091003 <huuphucnguyen73@gmail.com>
2024-03-24 19:57:11 +0700

    update

---
+++ cau1/src/ntu_63135101/DoiDonViDo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ntu_63135101;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * 
+ */
+public class DoiDonViDo {
+
+	
+	public static void main(String[] args) {
+	}
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
---
+++ cau1/src/ntu_63135101/form.java
@@ -0,0 +1,11 @@
+package ntu_63135101;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class form extends JFrame implements ActionListener{
+	private JTextField meterTextField;
+	private JTextField feetTextField;
+}

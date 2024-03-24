1529f8ee954819894f3e9cb67690bd2f8052f6c9
phuc091003 <huuphucnguyen73@gmail.com>
2024-03-24 20:22:24 +0700

    update

--- cau1/src/ntu_63135101/form.java
+++ cau1/src/ntu_63135101/form.java
@@ -8,4 +8,15 @@ import java.awt.event.ActionListener;
 public class form extends JFrame implements ActionListener{
 	private JTextField meterTextField;
 	private JTextField feetTextField;
+	
+	public form() {
+		setTitle("Chuyển đổi đơn vị đo");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets = new Insets(30,30,30,30);
+	}
 }

36448677c5fd25416329cafd78a4560a7eee0890
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-15 20:13:14 +0700

    .

---
+++ BaiTapJava/src/Lab2_Bai2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Lab2_Bai2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Nhap chieu dai: ");
+		double length = scanner.nextDouble();
+		
+		System.out.print("Nhap chieu rong: ");
+		double width = scanner.nextDouble();
+		
+		double chuvi = 2 * (length + width);
+		double dientich = length * width;
+		
+		double canhnho = Math.min(length, width);
+		
+		System.out.println("Chu vi: "+chuvi);
+		System.out.println("Dien tich: "+dientich);
+		System.out.println("Canh nho hon: "+canhnho);
+		
+		scanner.close();
+
+	}
+
+}

b92ade107c1a5d37cdf2079351ce7128145ef6f6
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-16 15:14:42 +0700

    update

---
+++ ViDuHuongDoiTuong/src/SanPham.java
@@ -0,0 +1,23 @@
+
+public class SanPham {
+	//Định nghĩa các thuộc tính
+	int maSP;
+	String tenSP;
+	String loaiSP;
+	String anhSP; // tên file ảnh
+	//Hàm tạo
+	public SanPham() {
+		super();
+	}
+	public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
+		// khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
+		super();
+		this.maSP = maSP;
+		this.tenSP = tenSP;
+		this.loaiSP = loaiSP;
+		this.anhSP = anhSP;
+	}
+	// Các getter, setter
+	
+
+}

dfff3c2fd5c474ce7749009e39bbc9ee30e650c9
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-16 19:40:07 +0700

    Update CT.java

--- ViDuHuongDoiTuong/src/CT.java
+++ ViDuHuongDoiTuong/src/CT.java
@@ -6,14 +6,18 @@ public class CT {
 		ArrayList<SanPham> dsSanPham;
 		dsSanPham = new ArrayList<SanPham>();
 		//Nhập 3 sản phẩm
-		SanPham sp1 = new SanPham(0, "Kem", "Giải khát", null);
+		//SanPham sp1 = new SanPham(0, "Kem", "Giải khát", null);
 		SanPham sp2 = new SanPham(1, "Cơm", "Đồ ăn", null);
 		SanPham sp3 = new SanPham(2, "Phở", "Đồ ăn", null);
 		// Thêm vào Arraylist
-		dsSanPham.add(sp1);
+		dsSanPham.add(new SanPham(0, "Kem", "Giải khát", null));
 		dsSanPham.add(sp2);
 		dsSanPham.add(sp3);
 		
+		for(int i = 0; i<dsSanPham.size(); i++) {
+			SanPham sp = dsSanPham.get(i);
+			System.out.println(sp.toString());
+		}
 	}
 
 }

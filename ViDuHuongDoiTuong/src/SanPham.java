a85e200d829da8bcf60284f63f46fd0682fa1fd2
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-16 15:20:09 +0700

    Update SanPham.java

--- ViDuHuongDoiTuong/src/SanPham.java
+++ ViDuHuongDoiTuong/src/SanPham.java
@@ -18,6 +18,35 @@ public class SanPham {
 		this.anhSP = anhSP;
 	}
 	// Các getter, setter
+	public int getMaSP() {
+		return maSP;
+	}
+	public void setMaSP(int maSP) {
+		this.maSP = maSP;
+	}
+	public String getTenSP() {
+		return tenSP;
+	}
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+	public String getAnhSP() {
+		return anhSP;
+	}
+	public void setAnhSP(String anhSP) {
+		this.anhSP = anhSP;
+	}
+	@Override
+	public String toString() {
+		return "[Mã SP=" + maSP + ", Tên SP=" + tenSP + ", Loại SP=" + loaiSP + "]";
+	}
+	
 	
 
 }

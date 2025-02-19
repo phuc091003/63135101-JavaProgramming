ccb0e5f1f65a9fceb693b983f7690bb4468217eb
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-19 15:22:16 +0700

    Update Lab2_Bai2.java

--- Lab2_Bai2_Phuongtrinhbac2/src/Lab2_Bai2.java
+++ Lab2_Bai2_Phuongtrinhbac2/src/Lab2_Bai2.java
@@ -24,8 +24,22 @@ public class Lab2_Bai2 {
 				System.out.println("Nghiem cua phuong trinh la : "+x);
 			}
 		} else {
-			
+			//Giai pt bac 2
+			double delta = b*b-4*a*c;
+			if(delta < 0) {
+				System.out.println("Phuong trinh vo nghiem");
+			}else if(delta == 0) {
+				double x= -b/(2*a);
+				System.out.println("Phuong trinh co nghiem kep: "+x);
+			}else {
+				double x1=(-b+Math.sqrt(delta))/(2*a);
+				double x2=(-b-Math.sqrt(delta))/(2*a);
+				System.out.println("Phuong trinh co 2 nghiem phan biet:");
+				System.out.println("x1= "+x1);
+				System.out.println("x2= "+x2);
+			}
 		}
+		scanner.close();
 
 	}
 

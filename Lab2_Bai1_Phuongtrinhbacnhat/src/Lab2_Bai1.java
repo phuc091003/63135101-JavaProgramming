dc4cdfb9677f48dc3913ae59af53195f15c8f432
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-19 15:07:29 +0700

    Update Lab2_Bai1.java

--- Lab2_Bai1_Phuongtrinhbacnhat/src/Lab2_Bai1.java
+++ Lab2_Bai1_Phuongtrinhbacnhat/src/Lab2_Bai1.java
@@ -6,6 +6,16 @@ public class Lab2_Bai1 {
 		//Nhap he so a va b
 		System.out.print("Nhap he so a: ");
 		double a = scanner.nextDouble();
+		System.out.print("Nhap he so b: ");
+		double b = scanner.nextDouble();
+		// Giai phuong trinh bac nhat
+		if(a==0) {
+			if(b==0) {
+				System.out.println("Phuong trinh co vo so nghiem");
+			}else {
+				System.out.println("Phuong trinh vo nghiem");
+			}
+		}
 
 	}
 

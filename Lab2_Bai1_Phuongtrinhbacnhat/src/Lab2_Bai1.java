9ea9d63f483347f4f345305790aa921cc1bcdd26
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-19 14:22:44 +0700

    .

---
+++ Lab2_Bai1_Phuongtrinhbacnhat/src/Lab2_Bai1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Lab2_Bai1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//Nhap he so a va b
+		System.out.print("Nhap he so a: ");
+		double a = scanner.nextDouble();
+
+	}
+
+}
